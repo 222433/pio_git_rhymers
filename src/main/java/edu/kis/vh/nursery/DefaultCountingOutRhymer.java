58a2c9bf74d102d3928cdac19195b0d3f889e026
222433 <222433@edu.p.lodz.pl>
2021-01-12 23:29:28 +0100

    accesory

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,7 @@ public class DefaultCountingOutRhymer {
 
 	private int[] numbers = new int[12];
 
-	public int total = -1;
+	private int total = -1;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -29,5 +29,9 @@ public class DefaultCountingOutRhymer {
 		if (callCheck())
 			return -1;
 		return numbers[total--];
+	}
+
+	public int getTotal() {
+		return total;
 	}
 }
