1c78d0d5e78505cc33c3f94b5c41b889ab86b237
222433 <222433@edu.p.lodz.pl>
2021-01-15 00:48:33 +0100

    Deleted Node class and reorganized IntLinkedList class

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,46 +2,81 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-	private Node last;
-	private int i;
+    private Node last;
+    private int i;
 
-	public void push(int i) {
-		if (last == null)
-			last = new Node(i);
-		else {
-			last.setNext(new Node(i)) ;
-			last.getNext().setPrev(last) ;
-			last = last.getNext();
-		}
-	}
+    public void push(int i) {
+        if (last == null)
+            last = new Node(i);
+        else {
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
+        }
+    }
 
-	public boolean isEmpty() {
-		return last == null;
-	}
+    public boolean isEmpty() {
+        return last == null;
+    }
 
-	public boolean isFull() {
-		return false;
-	}
+    public boolean isFull() {
+        return false;
+    }
 
-	public int top() {
-		if (isEmpty())
-			return -1;
-		return last.getValue();
-	}
+    public int top() {
+        if (isEmpty())
+            return -1;
+        return last.getValue();
+    }
 
-	public int pop() {
-		if (isEmpty())
-			return -1;
-		int ret = last.getValue();
-		last = last.getPrev();
-		return ret;
-	}
+    public int pop() {
+        if (isEmpty())
+            return -1;
+        int ret = last.getValue();
+        last = last.getPrev();
+        return ret;
+    }
 
-	public Node getLast() {
-		return last;
-	}
+    public Node getLast() {
+        return last;
+    }
 
-	public int getI() {
-		return i;
-	}
+    public int getI() {
+        return i;
+    }
+
+    private class Node {
+
+        private int value;
+        private Node prev, next;
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node(int i) {
+            value = i;
+        }
+
+    }
 }
